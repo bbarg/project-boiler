d9705994a2ea7981aa58bebb40755b4b7e28a678
Benjamin Barg <benbakerbarg@gmail.com>
2014-01-15 12:37:00 -0800

    Added directory structure with problem numbering

---
+++ 2_evenPythagoreanSum/evenPythagoreanSum.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+  int a,b;
+  int MAX = 4000000;
+  
+  int sum = 0;
+  a = 1;
+  b = 2;
+
+  while(a < MAX) {
+    // increment sum if a (term 1) is even
+    if(a % 2 == 0) sum += a;
+    // hold value of b (term 2)
+    int temp = b;
+    // calculate new term 2
+    b = a+b;
+    // make a the new term 1
+    a = temp;
+  }
+
+  printf("The sum of all the even-valued Fibonacci numbers less than %d is %d\n", MAX, sum);
+    return 0;
+}
